8f888cb2bc98c75b9a9134b7dd7023c01e2ea910
caovt <caovt@DESKTOP-PBKGOHJ>
2024-11-02 16:00:26 +0700

    Updated bikeshare.py and README.md

--- bikeshare.py
+++ bikeshare.py
@@ -12,6 +12,7 @@ def get_filters():
     
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # adding to documentation branch
     while True:
         city =input('ENTER THE CITY(Chicago, New York City,or Washington): ').lower()
         if city in cities:
